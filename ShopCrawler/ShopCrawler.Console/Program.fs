c4eeb05619d3914c4b3fffc3e6725e837da34fe2
daniilprohorov <daniilprohorov@gmail.com>
2020-10-25 15:14:34 +0300

    Add method to API for getting data from Keywords

--- ShopCrawler/ShopCrawler.Console/Program.fs
+++ ShopCrawler/ShopCrawler.Console/Program.fs
@@ -1,8 +1,11 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open ShopCrawler.Logic.API
 
 [<EntryPoint>]
 let main argv =
+    let appid = "-FindingD-PRD-fc9062648-8a125721"
     printfn "Hello World from F#!"
+    Console.Write(getRequestByKeywords appid "iphone")
     0 // return an integer exit code
