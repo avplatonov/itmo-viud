1a7c44abf0bed4036e9357892d9ee933d17be2d7
wonderLand <savamas47@mail.ru>
2020-11-11 11:50:48 +0300

    Init commit

---
+++ StackOverflow/StackOverflow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
